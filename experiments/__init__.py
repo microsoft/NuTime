d148e3f6b5c9e8d9bcfb629a151204fde1e3771e
xumengwen <xumengwen@microsoft.com>
2024-11-18 03:33:01 +0000

    merge experiments and config

---
+++ experiments/__init__.py
@@ -0,0 +1,3 @@
+from .base_exp import *
+from .cls_exp import *
+from .ssl_exp import *
--- run.sh
+++ run.sh
@@ -1,15 +1,18 @@
+DATA_DIR=<YOUR_DATA_DIR>
+CHECKPOINT_PATH=ckpt/checkpoint_bias9.pth
+
 # Pretrain
 python main.py --config_path configs/default_ssl.json \
     --dataset_dir $DATA_DIR --archives all-merged --force
 
 # Finetune from ckpt
 python main.py --config_path configs/default_finetune.json \
-    --load_checkpoint_path ckpt/checkpoint_bias9.pth \
+    --load_checkpoint_path $CHECKPOINT_PATH \
     --dataset_dir $DATA_DIR --archives ucr --force
 
-# Run few shot from ckpt
+# # Run few shot from ckpt
 python main.py --config_path configs/default_finetune.json \
-    --load_checkpoint_path ckpt/checkpoint_bias9.pth \ 
+    --load_checkpoint_path $CHECKPOINT_PATH \
     --dataset_dir $DATA_DIR --archives ucr --few_shot --force
 
 # Run from scratch
